64f285fe01ea2b399285863cb68fcc1db5ad9108
ddoniddoni <psdkei@naver.com>
2020-04-24 18:15:36 +0900

    Add wordRelay

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
-let word = "또니";
+const wordRelay = document.querySelector(".js__webgame-wordrelay");
+const wordRelayText = document.querySelector(".wordRelay__text");
+const wordRelayForm = document.querySelector(".wordRelay__form");
+const wordRelayInput = document.querySelector(".wordRelay__input");
+const wordRelayButton = document.querySelector(".wordRelay__btn");
+const wordRelayResult = document.querySelector(".wordRelay__result");
 
-while (true) {
-  let answer = prompt(word);
-  if (word[word.length - 1] === answer[0]) {
-    alert("딩동댕");
-    word = answer;
+wordRelayText.textContent = "또니";
+wordRelayButton.textContent = "입력";
+
+const wordRealy = (e) => {
+  e.preventDefault();
+  if (wordRelayText.textContent[wordRelayText.textContent.length - 1] === wordRelayInput.value[0]) {
+    wordRelayResult.textContent = "딩동댕";
+    wordRelayText.textContent = wordRelayInput.value;
+    wordRelayInput.value = "";
+    wordRelayInput.focus();
   } else {
-    alert("땡");
+    wordRelayResult.textContent = "땡";
+    wordRelayInput.focus();
+    wordRelayInput.value = "";
   }
-}
+};
+
+wordRelayForm.addEventListener("submit", wordRealy);
